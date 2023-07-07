fdd32ea2b9d560a182c20019a0903043d5cf93aa
Sanechek <rusakov@mail.ru>
2023-07-07 19:57:46 +0700

    create gendiff.js

---
+++ gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+
+program.parse();
