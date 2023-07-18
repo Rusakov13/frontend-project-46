cb1b0b82fdc6432867f01fbfb073ca1a9d0c126d
Sanechek <rusakov@mail.ru>
2023-07-18 14:50:49 +0700

    add tests

---
+++ tests/tests.index.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { expect, test } from '@jest/globals';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('test #1', () => {
+  const actual1 = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
+  expect(actual1).toEqual(readFile('expect.txt'));
+});
