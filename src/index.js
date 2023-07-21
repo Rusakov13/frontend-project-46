e28f5c80bc75362f53b2c07c58a09bfde17b4d30
Sanechek <rusakov@mail.ru>
2023-07-21 22:12:39 +0700

    removed parsing in a separate file

--- src/index.js
+++ src/index.js
@@ -1,62 +1,58 @@
 #!/usr/bin/env node
 import path from 'path';
+import process from 'process';
 import { readFileSync } from 'fs';
 import _ from 'lodash';
+import parser from './parsers.js';
 
-const getPath = (file) => path.resolve('__fixtures__', file);
+const getPath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
 
 const genDiff = (filepath1, filepath2) => {
   const absolutePath1 = getPath(filepath1);
   const absolutePath2 = getPath(filepath2);
 
-  const extension1 = path.extname(filepath1);
-  const extension2 = path.extname(filepath2);
+  const extension1 = path.extname(filepath1).slice(1);
+  const extension2 = path.extname(filepath2).slice(1);
 
   const obj1 = readFileSync(absolutePath1, 'utf-8');
   const obj2 = readFileSync(absolutePath2, 'utf-8');
 
-  let newObj1;
-  let newObj2;
-  if (extension1.slice(1) === 'json') {
-    newObj1 = JSON.parse(obj1);
-  }
-  if (extension2.slice(1) === 'json') {
-    newObj2 = JSON.parse(obj2);
-  }
+  const parseObj1 = parser(obj1, extension1);
+  const parseObj2 = parser(obj2, extension2);
 
-  const dataKeys1 = _.keys(newObj1);
-  const dataKeys2 = _.keys(newObj2);
+  const dataKeys1 = _.keys(parseObj1);
+  const dataKeys2 = _.keys(parseObj2);
   const sortedAllKeys = _.sortBy(_.union(dataKeys1, dataKeys2));
 
   const result = sortedAllKeys.map((key) => {
-    if (_.has(newObj1, key) && _.has(newObj2, key)) {
-      if (newObj1[key] === newObj2[key]) {
+    if (_.has(parseObj1, key) && _.has(parseObj2, key)) {
+      if (parseObj1[key] === parseObj2[key]) {
         return {
           key,
-          value: newObj1[key],
+          value: parseObj1[key],
           type: 'unchanged',
         };
       }
-      if (newObj1[key] !== newObj2[key]) {
+      if (parseObj1[key] !== parseObj2[key]) {
         return {
           key,
-          value1: newObj1[key],
-          value2: newObj2[key],
+          value1: parseObj1[key],
+          value2: parseObj2[key],
           type: 'changed',
         };
       }
     }
-    if (_.has(newObj1, key) && !_.has(newObj2, key)) {
+    if (_.has(parseObj1, key) && !_.has(parseObj2, key)) {
       return {
         key,
-        value: newObj1[key],
+        value: parseObj1[key],
         type: 'deleted',
       };
     }
-    if (!_.has(newObj1, key) && _.has(newObj2, key)) {
+    if (!_.has(parseObj1, key) && _.has(parseObj2, key)) {
       return {
         key,
-        value: newObj2[key],
+        value: parseObj2[key],
         type: 'added',
       };
     }
